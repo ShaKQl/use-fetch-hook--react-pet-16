7b411cca2fb1a112ac45db370c7947d07c0ed3d2
ShakQl <halimovsvhzod@gmail.com>
2026-01-08 21:13:59 +0500

    Scrolls top n bottom but need to switch to main component to work buttons overall on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
+
 import UseFetchHookTest from './test'
 import UseOnclickOutsideTest from './hooks/use-outside-click'
 import UseWindowResizeTest from './window-resize'
+import ScrollToTopAndBottom from './scroll-top-n-bttm'
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
 
   return (
     <>
     <UseFetchHookTest/>
     <UseOnclickOutsideTest/>
     <UseWindowResizeTest/>
+    <ScrollToTopAndBottom/>
     </>
   )
 }
--- src/test.jsx
+++ src/test.jsx
@@ -8,26 +8,32 @@ export default function UseFetchHookTest() {
     const { data, pending, error } = useFetch('https://dummyjson.com/products', {})
     return (
         <>
-            <div className="main main-box">
-                <h1>Use Fetch Hook</h1>
+            <div className="main">
+                <button>Scroll To Bottom</button>
 
-                {
-                    error
-                        ? <p>{error}</p>
-                        : pending
-                            ? <h2>Loading, Please Wait....</h2>
-                            : <div className="main-products">
-                                {
-                                    (data && data?.products)
-                                    && data.products.map(el => <p key={el.id}>{el.title}</p>)
+                <div className="main-box">
 
-                                }
-                            </div>
+                    <h1>Use Fetch Hook</h1>
+
+                    {
+                        error
+                            ? <p>{error}</p>
+                            : pending
+                                ? <h2>Loading, Please Wait....</h2>
+                                : <div className="main-products">
+                                    {
+                                        (data && data?.products)
+                                        && data.products.map(el => <p key={el.id}>{el.title}</p>)
+
+                                    }
+                                </div>
 
 
-                }
+                    }
 
-
+                </div>
+                
+                
 
             </div>
         </>
